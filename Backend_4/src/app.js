5c23a8c1bb21e8ee85a71a8ccc775ff18e5fd5c3
Adinath <adinathgaware23072003@gmail.com>
2026-09-01 16:53:06 +0530

    completed the api for my social pages test web app

--- Backend_4/src/app.js
+++ Backend_4/src/app.js
@@ -10,16 +10,21 @@ app.use(express.json()) // Middlewear used to
 const upload = multer({storage:multer.memoryStorage()})
 
 app.post('/create-post',upload.single('image'), async(req,res)=>{
+try{
 
     // We got a image file and we transfer it to the imagekit and they gave use a link in as a result 
     const result = await uploadFile(req.file.buffer)
     console.log(result)
-
+    
     // and we pass the result 
     const post = await postModel.create({
         image:result.url,
         caption:req.body.caption
     })
+}catch(error){
+    console.log(error)
+    return res.status(500).json({message:"something went wrong"})
+}
 
     return res.status(201).json({
         message:"post created successfully",
