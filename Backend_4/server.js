5146b9836b4885315f736fbb9d4ca7ba3d16802f
Adinath <adinathgaware23072003@gmail.com>
2026-09-03 22:32:22 +0530

    working on front-end

--- Backend_4/server.js
+++ Backend_4/server.js
@@ -6,6 +6,6 @@ const connectDB = require("./src/db/db.js");
 
 connectDB();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(3009, () => {
+  console.log("Server is running on port 3009");
 });
--- Backend_4/src/app.js
+++ Backend_4/src/app.js
@@ -7,11 +7,16 @@ const app = express();
 
 app.use(express.json()) // Middlewear used to
 
-const upload = multer({storage:multer.memoryStorage()})
+const upload = multer({
+    storage:multer.memoryStorage(), 
+    limits:{fileSize:10*1024*1024} // 10MB image size Limit
+})
 
 app.post('/create-post',upload.single('image'), async(req,res)=>{
 try{
-
+    if(!req.file){
+        return res.status(400).json({message:"no image file provided"})
+    }
     // We got a image file and we transfer it to the imagekit and they gave use a link in as a result 
     const result = await uploadFile(req.file.buffer)
     console.log(result)
@@ -21,16 +26,50 @@ try{
         image:result.url,
         caption:req.body.caption
     })
-}catch(error){
-    console.log(error)
-    return res.status(500).json({message:"something went wrong"})
-}
-
-    return res.status(201).json({
+     return res.status(201).json({
         message:"post created successfully",
         data:post
     })
+}catch(error){
+    console.log(error)
+    return res.status(500).json({message:"something went wrong"})
+}  
+})
 
+app.get("/posts",async(req,res)=>{
+    try{
+        const posts = await postModel.find()
+        return res.status(200).json({
+            message:"we got the posts",
+            data:posts
+        })
+    }catch(error){
+            return res.status(500).json({message:"something went wrong"})
+        }
+})
+
+app.delete("/posts/:id",async(req,res)=>{
+    try{
+        const id = req.params.id
+        const post = await postModel.findByIdAndDelete(id)
+
+        // check if the post existed and was actually deleted
+        if(!post){
+            return res.status(404).json({
+                message:"post not found"
+            });
+        }
+
+        return res.status(200).json({
+            message:"post deleted successfully",
+            data:post
+        })
+    }catch(error){
+         return res.status(500).json({
+            message: "Something went wrong",
+            error: error.message // Optional: helpful for debugging development environments
+        });
+    }
 })
 
 module.exports = app;
